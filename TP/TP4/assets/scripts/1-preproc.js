dbd1754f991e97e0a309b7822b983722a5707948
mrnaia <marina.menghetti@gmail.com>
2019-02-07 09:08:15 -0500

    one line getMatr

--- TP/TP4/assets/scripts/1-preproc.js
+++ TP/TP4/assets/scripts/1-preproc.js
@@ -50,9 +50,8 @@ function domainY(y, currentData) {
 function getMatrix(data) {
   // TODO: Calculer la matrice d'adjacence pour créer le diagramme à cordes.
   var matr = [];
-  data.forEach(d => {
-    matr.push(d.destinations.map(c => c.count));
-  })
+  data.forEach(d => matr.push(d.destinations.map(c => c.count)))
+  console.log(matr);
   return matr;
 }
 
