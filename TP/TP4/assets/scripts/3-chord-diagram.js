ab65ce407540b177598bac071dc81dfbd839144a
mrnaia <marina.menghetti@gmail.com>
2019-02-14 09:52:15 -0500

    commentaires choard diagram file

--- TP/TP4/assets/scripts/3-chord-diagram.js
+++ TP/TP4/assets/scripts/3-chord-diagram.js
@@ -25,21 +25,23 @@ function createGroups(g, data, layout, arc, color, total, formatPercent) {
      - Tronquer les noms des stations de BIXI qui sont trop longs (Pontiac et Métro Mont-Royal).
      - Afficher un élément "title" lorsqu'un groupe est survolé par la souris.
   */
+
+  //Create the group
   var group = g.selectAll("g")
     .data(layout.groups)
     .enter()
     .append("g")
     .attr("class","group")
 
+  //Create the background path group
   group.append("path")
-    .attr("fill", d => {
-      return color(data[d.index].name)
-    })
-    .attr("d",arc)
-    .property("id",d => data[d.index].name);
+    .attr("fill", d => color(data[d.index].name))
+    .attr("d", arc)
+    .property("id", d => data[d.index].name);
 
+  //Create the text for the group
   group.append("text")
-    .style("font-size","9pt")
+    .style("font-size", "9pt")
     .attr("dy", 17)
     .attr("dx", 10)
     .append("textPath")
@@ -52,9 +54,10 @@ function createGroups(g, data, layout, arc, color, total, formatPercent) {
       if(text === "Pontiac / Gilford"){
         text = "Pontiac"
       }
-      return text
+      return text;
     })
 
+  //Create the tip title
   group.append("title")
     .text(d => data[d.index].name + ": " + formatPercent(d.value/total) + " des départs")
 }
@@ -77,37 +80,27 @@ function createChords(g, data, layout, path, color, total, formatPercent) {
      - Créer les cordes du diagramme avec une opacité de 80%.
      - Afficher un élément "title" lorsqu'une corde est survolée par la souris.
   */
-  console.log(data);
-  g.append("g")
+  g.append("g") //Chords
     .selectAll("path")
     .data(layout)
     .enter()
     .append("path")
     .attr("d", path)
     .attr("class", "chord")
-    .attr("fill", function(d){
-      //console.log(data[d.source.index].destinations[d.target.index]);
-      let nbDeparts_source_vers_target =data[d.source.index].destinations[d.target.index].count;
-      let nbDeparts_target_vers_src =data[d.target.index].destinations[d.source.index].count ;
-      if(nbDeparts_source_vers_target>=nbDeparts_target_vers_src){
-        return color(data[d.source.index].name);
-      }
-      else{
-        return color(data[d.target.index].name);
-      }
+    .attr("fill", d => { //Chords color
+      let nbDeparts_source_vers_target = data[d.source.index].destinations[d.target.index].count;
+      let nbDeparts_target_vers_src = data[d.target.index].destinations[d.source.index].count;
+      if (nbDeparts_source_vers_target >= nbDeparts_target_vers_src) return color(data[d.source.index].name);
+      else return color(data[d.target.index].name);
     })
-    .append("title")
+    .append("title") //Tip title
     .html(d => {
-      let nbDeparts_source_vers_target =data[d.source.index].destinations[d.target.index].count;
-      let nbDeparts_target_vers_src =data[d.target.index].destinations[d.source.index].count ;
+      let nbDeparts_source_vers_target = data[d.source.index].destinations[d.target.index].count;
+      let nbDeparts_target_vers_src = data[d.target.index].destinations[d.source.index].count;
       let source = data[d.source.index].name;
       let target = data[d.target.index].name;
       return source + " &rarr; " + target + ": " + formatPercent(nbDeparts_source_vers_target/total) + "\n" + target + " &rarr; " + source + ": " + formatPercent(nbDeparts_target_vers_src/total);
     })
-    //revoir couleur, title
-/*  g.selectAll("path")
-    .append("title")
-    .text(d => d)*/
 }
 
 /**
@@ -121,6 +114,7 @@ function initializeGroupsHovered(g) {
        opacité de 80%. Toutes les autres cordes doivent être affichées avec une opacité de 10%.
      - Rétablir l'affichage du diagramme par défaut lorsque la souris sort du cercle du diagramme.
   */
+  //Define mouseover
   g.selectAll(".group").on("mouseover", function(d){
     d3.selectAll(".chord").attr("class", function(dChord){
       if(!(dChord.source.index === d.index || dChord.target.index === d.index)){
@@ -130,6 +124,7 @@ function initializeGroupsHovered(g) {
       }
     });
   })
+  //Define mouseleave
   g.on("mouseleave", d => {
     d3.selectAll(".chord").attr("class", "chord");
   })
