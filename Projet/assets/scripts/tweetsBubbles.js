58ffdfa02caf76433a15f7bab93ba1708a452184
Gautier Dervaux <gautier_dervaux@hotmail.com>
2019-03-14 11:19:31 -0400

    fix in html

--- Projet/assets/scripts/tweetsBubbles.js
+++ Projet/assets/scripts/tweetsBubbles.js
@@ -1,1 +1,9 @@
-"use strict"
+"use strict";
+function createBubbleChart(g,sources){
+
+  svg.selectAll("circle")
+  .data(sources)
+  .enter()
+  .append("circle")
+  .attr("r", (d) => 1+d.retweet_count)
+}
