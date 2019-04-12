e10b2098288fc69a296ff9dbfef964e8d305896d
Marina Menghetti <marina.menghetti@gmail.com>
2019-04-11 20:15:44 -0400

    rectifié emplacement tweets svg avec search bar

--- Projet/assets/scripts/searchMediasBar.js
+++ Projet/assets/scripts/searchMediasBar.js
@@ -5,6 +5,12 @@ function initSearchMediasBar(mediaSources, mediaChartGroup, tweetsChartGroup, tw
   $("#mediaTags").autocomplete({
     source: mediaFullNames,
     select: function(a, b) {
+      updateFilterCheck();
+      tweetsChartGroup.attr("transform",""); //reset translation of tweet group
+      d3.select(".annotations")
+        .transition()
+        .duration(500)
+        .attr("opacity","0")
       let name = (mediaSources.filter(function(d){return d.fullName == b.item.value}))[0].name;
       selectNewMedia(name, b.item.value, mediaChartGroup, tweetsChartGroup, tweetColorScale, tweetSources);
     },
